bc101940c51f84dafa145a25523b1cf53ca3fe5a
NICE_SYSTEMS\eliranda <eliranda@nice.com>
2018-03-07 18:49:40 +0200

    packaging as WAR

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import com.demo.tweets.core.SearchTweets;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MyController {
@@ -12,9 +15,8 @@ public class MyController {
     @RequestMapping(value="/getTweets/{name}", method = RequestMethod.GET)
     public @ResponseBody
     String hell(@PathVariable("name") String name) {
-        //SearchTweets searchTweers = new SearchTweets();
-        //List<String> top5 = searchTweers.getTop5(name);
-        //return String.join("\n", top5);
-        return  "Boo000000";
+        SearchTweets searchTweers = new SearchTweets();
+        List<String> top5 = searchTweers.getTop5(name);
+        return String.join("\n", top5);
     }
 }
